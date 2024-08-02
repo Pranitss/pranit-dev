98359cb66bb84453c6b9eb896d1bc2d1b5a658d4
pranit sarode <93464140+Pranitss@users.noreply.github.com>
2024-08-02 20:59:24 +0530

    Update console.js

--- console.js
+++ console.js
@@ -2,3 +2,11 @@
 console.log('Hello World');
 console.log('Hello World');//2nd time
 console.log('Hello World');
+const num1 = 5;
+const num2 = 3;
+
+// add two numbers
+const sum = num1 + num2;
+
+// display the sum
+console.log('The sum of ' + num1 + ' and ' + num2 + ' is: ' + sum);
